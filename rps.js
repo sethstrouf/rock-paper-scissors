0e0a3d62e702228ca6a499813f2348f06adf44e3
Seth Strouf <seth.strouf@doane.edu>
2020-12-26 14:07:32 -0600

    Wrote script logic, game is playable

--- rps.js
+++ rps.js
@@ -1,92 +1,120 @@
 function computerPlay () {
     let choice = Math.floor(Math.random()*3);
     switch (choice) {
-        case 0: return "rock";
-        case 1: return "paper";
-        case 2: return "scissors";
+        case 0: return "rock".toUpperCase();
+        case 1: return "paper".toUpperCase();
+        case 2: return "scissors".toUpperCase();
     }
+}
+
+function drawComputerChoice(computerChoice) {
+    if(computerChoice==="ROCK") {
+        computerDisplay.className = "fas fa-hand-rock computer-box";
+    } else if(computerChoice==="PAPER") {
+        computerDisplay.className = "fas fa-hand-paper computer-box";
+    } else if(computerChoice==="SCISSORS") {
+        computerDisplay.className = "fas fa-hand-scissors computer-box";
+    }
+
+    computerDisplay.classList.add("clicked");
+    setTimeout(function () {
+        computerDisplay.classList.remove("clicked");
+    }, 100);
+
+    computerDiv.append(computerDisplay);
+}
+
+function compareHands(playerChoice,computerChoice) {
+    if (playerChoice === computerChoice) {
+        console.log("YOU TIE!")
+    } else if (playerChoice==="ROCK") {
+        if (computerChoice==="PAPER") {
+            playerLoss();
+        } else {
+            playerWin();
+        }
+      }
+    else if (playerChoice==="PAPER") {
+        if (computerChoice==="SCISSORS") {
+            playerLoss();
+        } else {
+            playerWin();
+        }
+    }
+    else if (playerChoice==="SCISSORS") {
+        if (computerChoice==="ROCK") {
+            playerLoss();
+        } else {
+            playerWin();
+        }
+    }
+}
+
+function updateScoreBoard() {
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
+}
+
+function endGame() {
+    if(playerScore>computerScore) {
+        finalMessage.textContent = "YOU WON!!!";
+    } else if (computerScore>playerScore) {
+        finalMessage.textContent = "YOU LOST!!!";
+    } else {
+        finalMessage.textContent = "YOU TIED!!";
+    }
+    finalMessageDisplay.append(finalMessage);
+    newGame = true;
+    gameCounter = 0;
 }
 
 function playerLoss(computer) {
-    console.log("Computer chose "+computer+"! YOU LOSE!");
-            return "LOSS";
+    computerScore++;
 }
 
 function playerWin(computer) {
-    console.log("Computer chose "+computer+"! YOU WIN!");
-            return "WIN";
+    playerScore++;
 }
 
-function giveScore(playerScore, computerScore) {
-    if (playerScore===computerScore) {
-        console.log(`YOU TIED THE GAME WITH A SCORE OF ${playerScore}`);
-    } else if (playerScore > computerScore) {
-        console.log(`YOU WON THE GAME ${playerScore} to ${computerScore}!`);
-    } else {
-        console.log(`YOU LOST THE GAME ${playerScore} to ${computerScore}!`);
+//this is where it all happens after the click!
+function playRound (playerChoice) {
+
+    let computerChoice = computerPlay();
+    drawComputerChoice(computerChoice);
+    compareHands(playerChoice,computerChoice);
+    updateScoreBoard();
+
+    gameCounter++;
+
+    if(gameCounter>=5) {
+        endGame();
     }
 }
 
-function playRound () {
-    let player = prompt("What do you pick?", "Type 'rock' 'paper' or 'scissors'");
-    player = player.toUpperCase();
-    let computer = computerPlay();
-    computer = computer.toUpperCase();
-
-    if (player === computer) {
-        console.log("It's a tie!");
-        return "TIE";
-    } else if (player==="ROCK") {
-        if (computer==="PAPER") {
-            return playerLoss(computer);
-        } else {
-            return playerWin(computer);
-        }
-      }
-    else if (player==="PAPER") {
-        if (computer==="SCISSORS") {
-            return playerLoss(computer);
-        } else {
-            return playerWin(computer);
-        }
-    }
-    else if (player==="SCISSORS") {
-        if (computer==="ROCK") {
-            return playerLoss(computer);
-        } else {
-            return playerWin(computer);
-        }
-    }  
-}
-
-function game(numOfGames) {
-
-    let playerScore = 0;
-    let computerScore = 0;
-
-    while (numOfGames > 0) {
-
-        let winState = playRound();
-        
-        if (winState==="WIN") {
-            playerScore++;
-        } else if (winState==="LOSS") {
-            computerScore++;
-        }
-
-        numOfGames--;
-    }
-
-    giveScore(playerScore, computerScore);
-}
-
-game(0);
-
-const boxes = document.querySelectorAll("[data-choice]");
+//global vars - refactor when I'm better at this
+let boxes = document.querySelectorAll("[data-choice]");
+let computerDiv = document.querySelector("#computer-choice");
+let computerDisplay = document.querySelector("i.computer");
+let playerScoreDisplay = document.querySelector(".player-score");
+let computerScoreDisplay = document.querySelector(".computer-score");
+let finalMessageDisplay = document.querySelector(".final-message");
+let finalMessage = document.createElement("h3");
+let playerScore = 0;
+let computerScore = 0;
+let winState = "";
+let gameCounter = 0;
+let newGame = true;
 
 boxes.forEach(box => {
     box.addEventListener("click", () => {
-        console.log("click click");
-        box.classList.add("clicked");
+        if(newGame) {
+            playerScore = 0;
+            computerScore = 0;
+            finalMessage.textContent = " ";
+            finalMessageDisplay.append(finalMessage);
+            newGame = false;
+        }
+        let playerChoice = box.dataset.choice.toUpperCase();
+        winState = playRound(playerChoice);
     });
 });
